b3c9c0fb19a846b8afbbc662fb317640e960ad63
Ethan Truong <etruong42@gmail.com>
2012-05-17 12:48:48 -0400

    essentially finished; add README, more data, allow random generation to not repeat

---
+++ pclorem/pclorem.js
@@ -0,0 +1,111 @@
+if( typeof PCLOREM === "undefined" || !PCLOREM ) { var PCLOREM = {}; }
+
+(function() {
+	this.populate = function() {
+		var startTag = "";
+		var endTag = "";
+		var wrapTags = $("#wrapTag").val();
+		if($("#wrapCheckbox").is(':checked')){
+			startTag = wrapTags;
+			endTag = wrapTags[0] + "/" + wrapTags.substring(1);
+		}
+		this.clearResults();
+		//$(PCLOREM.paragraphs[0]).each(function(index, value){
+		//	PCLOREM.addParagraphToResults(startTag + value + endTag);
+		//});
+		
+		var numParagraphs = this.getNumParagraphToGenerate();
+		
+		for(var x = 0; x < numParagraphs; x++) {
+			var selectedIndices = this.getSelectedIndices();
+			var selectionIndex = this.rand(0, selectedIndices.length-1);
+			var paragraphIndex = this.rand(0, this.paragraphs[selectionIndex].length-1);
+			PCLOREM.addParagraphToResults(startTag + this.paragraphs[selectionIndex][paragraphIndex] + endTag);
+		}
+		
+	}
+	
+	this.rand = function(lower, upper) {
+		return Math.floor(Math.random()*(upper-lower+1)) + lower;
+	}
+	
+	this.addParagraphToResults = function(paragraph) {
+		if(PCLOREM.getResultsElement().val()) {
+			PCLOREM.getResultsElement().val($("#results").val() + "\n\n");
+		}
+		
+		PCLOREM.getResultsElement().val($("#results").val() + paragraph.toString())
+	}
+	
+	this.clearResults = function() {
+		this.getResultsElement().val("");
+	}
+	
+	this.getResultsElement = function() {
+		return $("#results");
+	}
+
+	this.getParagraphs = function() {
+		var paragraphs = "";
+		var startTag = "";
+		var endTag = "";
+		var wrapTags = $("#wrapTag").val();
+		if($("#wrapCheckbox").is(':checked')){
+			startTag = wrapTags;
+			endTag = wrapTags[0] + "/" + wrapTags.substring(1);
+		}
+		$(PCLOREM.paragraphs[0]).each(function(index, value){paragraphs += startTag + value + endTag + "\n\n";});
+		paragraphs = paragraphs.substring(0, paragraphs.length-2);
+		return paragraphs;
+	}
+	
+	this.getNumParagraphToGenerate = function() {
+		return $("#txtNumParagraphs").val();
+	}
+	
+	//could just get index of selected children, but want to decouple ordering
+	//of view from ordering of model
+	this.getSelectedIndices = function() {
+		var indices = new Array();
+		var selectedbooks = this.getSelectedBooks();
+		$(PCLOREM.titles).each(function(index, value) {
+			if($.inArray(this.toString(), selectedbooks) > -1) {
+				indices.push(index);
+			}
+		});
+		return indices;
+	}
+	
+	this.getSelectedBooks = function() {
+		var selectedBooks = new Array();
+		var selectedBookElements = this.getBookSelectionElements();
+		$(selectedBookElements).each(function(index, value) {
+			if($(value).hasClass("active")) {
+				selectedBooks.push($(value).html());
+			}
+		});
+		return selectedBooks;
+	}
+	
+	this.getBookSelectionElements = function() {
+		return this.getBookSelectionRootElement().children("button");
+	}
+	
+	this.getBookSelectionRootElement = function() {
+		return $("#bookselections");
+	}
+	
+	this.initialize = function() {
+		$(this.titles).each(function(index, value) {
+			$("#bookselections").append(PCLOREM.createAndGetCheckboxForTitle(value))
+		});		
+	}
+	
+	this.createAndGetCheckboxForTitle = function(booktitle) {
+		return "<button class=\"btn active\">"+booktitle+"</button>"
+	}
+}).apply(PCLOREM);
+
+ $(document).ready(function () {
+	PCLOREM.initialize();
+ });
